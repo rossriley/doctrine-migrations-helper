2f3bd1c40fa5cb18312ca91dbf765cf8b7d50465
Ross Riley <riley.ross@gmail.com>
2014-02-26 11:15:47 +0000

    alias Doctrine namespace to Command

--- src/CommandSet.php
+++ src/CommandSet.php
@@ -4,7 +4,7 @@ namespace DoctrineMigrationsHelper;
 use Doctrine\DBAL\Migrations\Configuration\Configuration;
 use Doctrine\DBAL\Connection;
 
-use Doctrine\DBAL\Migrations\Tools\Console\Command;
+use Doctrine\DBAL\Migrations\Tools\Console\Command as Command;
 
 /**
  * CommandSet Class
