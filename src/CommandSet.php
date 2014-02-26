97d1f9aee056ea22c211f828163ce0ba54405c4e
Ross Riley <riley.ross@gmail.com>
2014-02-26 12:21:27 +0000

    fix to string expansion syntax

--- src/CommandSet.php
+++ src/CommandSet.php
@@ -45,7 +45,7 @@ class CommandSet
     public function getCommands() {
         $commands = [];
         foreach($this->commands as $command) {
-            $class = "Doctrine\DBAL\Migrations\Tools\Console\Command\$commandCommand";
+            $class = "Doctrine\DBAL\Migrations\Tools\Console\Command\\".$command."Command";
             $obj = new $class;
             $obj->setMigrationConfiguration($this->configuration);
             $commands[]=$obj;
