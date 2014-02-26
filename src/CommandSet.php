8367edea55a546c58591f9dcd590202dba945c12
Ross Riley <riley.ross@gmail.com>
2014-02-26 10:44:05 +0000

    initial setup and first version of project

---
+++ src/CommandSet.php
@@ -0,0 +1,57 @@
+<?php
+namespace DoctrineMigrationsHelper;
+
+use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\DBAL\Connection;
+
+use Doctrine\DBAL\Migrations\Tools\Console\Command;
+
+/**
+ * CommandSet Class
+ *
+ * Acts as a helper to produce console ready commands from a custom Migrations Configuration
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ **/
+class CommandSet
+{
+
+    public $commands = [
+        "Diff",
+        "Execute",
+        "Generate",
+        "Latest",
+        "Migrate",
+        "Status",
+        "Version"
+    ];
+
+    public $configuration;
+
+    /**
+     * Constructor
+     * @param $connection - Requires a Doctrine DBAL Connection
+     **/
+    public function __construct(Connection $connection, $namespace = false, $directory = false)
+    {
+        $this->configuration = new Configuration($connection);
+    }
+
+
+    /**
+     * getCommands function
+     *
+     * @return Array[Command]
+     **/
+    public function getCommands() {
+        $commands = [];
+        foreach($this->commands as $command) {
+            $class = "Command\\".$command;
+            $obj = new $class;
+            $obj->setMigrationConfiguration($this->configuration);
+            $commands[]=$obj;
+        }
+        return $commands;
+    }
+
+}
